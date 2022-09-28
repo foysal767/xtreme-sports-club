2eda0def1f523fa42897e4996ce4745b74bc9c00
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-09-28 11:48:44 -0700

    activities component added

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
+import "./Activities.css";
 
 const Activities = () => {
     const [activities, setActivities] = useState([]);
@@ -9,12 +10,18 @@ const Activities = () => {
         .then(data => setActivities(data))
     }, [])
     return (
-        <div>
-            <h1>This is Activities</h1>
+        <div className='page-container'>
+            <div className='activities-container'>
             {
                 activities.map(activity => <Cart
+                key={activity.id}
                 activity={activity}></Cart>)
             }
+            </div>
+            <div className='personal-info'>
+                <h4>Mohammad Foysal Uddin</h4>
+                <p>Web Developer</p>
+            </div>
         </div>
     );
 };
